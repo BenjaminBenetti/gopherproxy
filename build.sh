db9f65ea9bdce9fd0605ccd394dca40c98f8da96
Benjamin Benetti <ben@bbenetti.ca>
2025-01-27 02:30:02 +0000

    update build script to also buld arm, and set GIN mode to release in prod image

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+if [ -z "$1" ]; then
+  echo "Error: Tag argument is required"
+  exit 1
+fi
+TAG="$1"
 
-go build -a -v -o ./bin/gopherproxy ./cmd/gopherproxyclient
-go build -a -v -o ./bin/gopherproxyserver ./cmd/gopherproxyserver
+# build x86
+go build -a -v -o ./bin/gopherproxy-${TAG}.x86 ./cmd/gopherproxyclient
+go build -a -v -o ./bin/gopherproxyserver-${TAG}.x86 ./cmd/gopherproxyserver
+
+# build arm
+GOARCH=arm64 go build -a -v -o ./bin/gopherproxy-${TAG}.arm ./cmd/gopherproxyclient
+GOARCH=arm64 go build -a -v -o ./bin/gopherproxyserver-${TAG}.arm ./cmd/gopherproxyserver
